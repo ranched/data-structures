569a9073b0f8bd407b9891699b896bcbc3297de6
Jason McLaughlin <jason@Jasons-MacBook-Pro.local>
2017-11-15 19:14:08 -0600

    Queue-Shared reports size 0 for new queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
+var extend = function(obj, methods){
+  for(var key in methods){
+    obj[key] = methods[key];
+  }
+}
+
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.queueHead = 0;
+  queue.queueRear = -1;
+  queue.length = 0;
+  extend(queue, queueMethods);
+  return queue;
 };
 
-var queueMethods = {};
+ var queueMethods = {
+  resize: function(){
 
+  },
 
+  enqueue: function(){
+
+  },
+
+  dequeue: function(){
+
+  },
+
+  size: function(){
+    return this.length;
+  }
+};

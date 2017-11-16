4a9341e89a5c79d31ecce20008e9119c9ccc6c65
Jason McLaughlin <jasonstephenm@gmail.com>
2017-11-16 17:25:30 -0600

    Passes linked list-returns value of former head

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,26 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if(!list.head && !list.tail) {
+      list.tail = list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if(!list.head){
+      return
+    }
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
+  
   };
 
   return list;

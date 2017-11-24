5c80183e7034b2ce5db191724da69443c38fb581
Jason McLaughlin <jasonstephenm@gmail.com>
2017-11-24 13:18:21 -0600

    Add underscore to functional stack private properties

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,38 +2,38 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var _storage = {};
   //set the initial stack size to 0
-  var size = 0;
+  var _size = 0;
 
   //add values to the stack
   someInstance.push = function(value) {
     //increase the stack size by one
-    size++;
+    _size++;
     //put the new value at the top of the stack
-    storage[size] = value;
+    _storage[_size] = value;
   };
 
   //remove values from the stack
   someInstance.pop = function() {
     //if the stack is not empty
-    if (size !== 0) {
+    if (_size !== 0) {
       //set temp variable equal to top value
-      var item = storage[size];
+      var item = _storage[_size];
       //delete the top stack item
-      delete storage[size];
+      delete _storage[_size];
       //decrement the stack size counter
-      size--;
+      _size--;
       return item;
     }
   };
 
   someInstance.getLastRemovedItem = function() {
-    return storage[size + 1];
+    return _storage[_size + 1];
   };
 
   someInstance.size = function() {
-    return size;
+    return _size;
   };
 
   return someInstance;
